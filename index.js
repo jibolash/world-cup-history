6202330db9c52ad34851f57442bc17ce94fe5cae
Jibola Sogbein <ajibolasogbein@gmail.com>
2018-05-05 10:51:09 -0500

    add total attendance and number of matches

--- index.js
+++ index.js
@@ -3,101 +3,141 @@ var worldCups = {
         hostCountry: 'Uruguay',
         winner: 'Uruguay',
         runnerUp: 'Argentina',
+        totalAttendance: 590549,
+        numberOfMatches: 18
     },
     '1934': {
         hostCountry: 'Italy',
         winner: 'Italy',
         runnerUp: ' Czechoslovakia',
+        totalAttendance: 363000,
+        numberOfMatches: 17
     },
     '1938': {
         hostCountry: 'France',
         winner: 'Italy',
         runnerUp: 'Hungary',
+        totalAttendance: 375000,
+        numberOfMatches: 18
     },
     '1950': {
         hostCountry: 'Brazil',
         winner: 'Uruguay',
         runnerUp: 'Brazil',
+        totalAttendance: 1045246,
+        numberOfMatches: 22
     },
     '1954': {
         hostCountry: 'Switzerland',
         winner: 'West Germany',
         runnerUp: 'Hungary',
+        totalAttendance: 768607,
+        numberOfMatches: 26
     },
     '1958': {
         hostCountry: 'Sweden',
         winner: 'Brazil',
         runnerUp: 'Sweden',
+        totalAttendance: 819810,
+        numberOfMatches: 35
     },
     '1962': {
         hostCountry: 'Chile',
         winner: 'Brazil',
         runnerUp: 'Czechoslovakia',
+        totalAttendance: 893172,
+        numberOfMatches: 32
     },
     '1966': {
         hostCountry: 'England',
         winner: 'England',
         runnerUp: 'West Germany',
+        totalAttendance: 1563135,
+        numberOfMatches: 32
     },
     '1970': {
         hostCountry: 'Mexico',
         winner: 'Brazil',
         runnerUp: 'Italy',
+        totalAttendance: 1603975,
+        numberOfMatches: 32
     },
     '1974': {
         hostCountry: 'West Germany',
         winner: 'West Germany',
         runnerUp: 'Netherlands',
+        totalAttendance: 1865753,
+        numberOfMatches: 38
     },
     '1978': {
         hostCountry: 'Argentina',
         winner: 'Argentina',
         runnerUp: 'Netherlands',
+        totalAttendance: 1545791,
+        numberOfMatches: 38
     },
     '1982': {
         hostCountry: 'Spain',
         winner: 'Italy',
         runnerUp: 'West Germany',
+        totalAttendance: 2109723,
+        numberOfMatches: 52
     },
     '1986': {
         hostCountry: 'Mexico',
         winner: 'Argentina',
         runnerUp: 'West Germany',
+        totalAttendance: 239403,
+        numberOfMatches: 52
     },
     '1990': {
         hostCountry: 'Italy',
         winner: 'West Germany',
         runnerUp: 'Argentina',
+        totalAttendance: 2516215,
+        numberOfMatches: 52
     },
     '1994': {
         hostCountry: 'United States',
         winner: 'Brazil',
         runnerUp: 'Italy',
+        totalAttendance: 3587538,
+        numberOfMatches: 52
     },
     '1998': {
         hostCountry: 'France',
         winner: 'France',
         runnerUp: 'Brazil',
+        totalAttendance: 2785100,
+        numberOfMatches: 64
     },
     '2002': {
         hostCountry: 'South Korea, Japan',
         winner: 'Brazil',
         runnerUp: 'Germany',
+        totalAttendance: 2705197,
+        numberOfMatches: 64
     },
     '2006': {
         hostCountry: 'Germany',
         winner: 'Italy',
         runnerUp: 'France',
+        totalAttendance: 3359439,
+        numberOfMatches: 64
     },
     '2010': {
         hostCountry: 'South Africa',
         winner: 'Spain',
         runnerUp: 'Netherlands',
+        totalAttendance: 3178856,
+        numberOfMatches: 64
     },
     '2014': {
         hostCountry: 'Brazil',
         winner: 'Germany',
         runnerUp: 'Argentina',
+        totalAttendance: 3429873,
+        numberOfMatches: 64
     },
 };
 
