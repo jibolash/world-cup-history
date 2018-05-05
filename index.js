85fa16dc0ba4e9713afba334cc2d5748dbdda0dd
Jibola Sogbein <ajibolasogbein@gmail.com>
2018-05-05 01:08:36 -0500

    initial commit with basic world cup data

---
+++ index.js
@@ -0,0 +1,105 @@
+var worldCups = {
+    '1930': {
+        hostCountry: 'Uruguay',
+        winner: 'Uruguay',
+        runnerUp: 'Argentina',
+    },
+    '1934': {
+        hostCountry: 'Italy',
+        winner: 'Italy',
+        runnerUp: ' Czechoslovakia',
+    },
+    '1938': {
+        hostCountry: 'France',
+        winner: 'Italy',
+        runnerUp: 'Hungary',
+    },
+    '1950': {
+        hostCountry: 'Brazil',
+        winner: 'Uruguay',
+        runnerUp: 'Brazil',
+    },
+    '1954': {
+        hostCountry: 'Switzerland',
+        winner: 'West Germany',
+        runnerUp: 'Hungary',
+    },
+    '1958': {
+        hostCountry: 'Sweden',
+        winner: 'Brazil',
+        runnerUp: 'Sweden',
+    },
+    '1962': {
+        hostCountry: 'Chile',
+        winner: 'Brazil',
+        runnerUp: 'Czechoslovakia',
+    },
+    '1966': {
+        hostCountry: 'England',
+        winner: 'England',
+        runnerUp: 'West Germany',
+    },
+    '1970': {
+        hostCountry: 'Mexico',
+        winner: 'Brazil',
+        runnerUp: 'Italy',
+    },
+    '1974': {
+        hostCountry: 'West Germany',
+        winner: 'West Germany',
+        runnerUp: 'Netherlands',
+    },
+    '1978': {
+        hostCountry: 'Argentina',
+        winner: 'Argentina',
+        runnerUp: 'Netherlands',
+    },
+    '1982': {
+        hostCountry: 'Spain',
+        winner: 'Italy',
+        runnerUp: 'West Germany',
+    },
+    '1986': {
+        hostCountry: 'Mexico',
+        winner: 'Argentina',
+        runnerUp: 'West Germany',
+    },
+    '1990': {
+        hostCountry: 'Italy',
+        winner: 'West Germany',
+        runnerUp: 'Argentina',
+    },
+    '1994': {
+        hostCountry: 'United States',
+        winner: 'Brazil',
+        runnerUp: 'Italy',
+    },
+    '1998': {
+        hostCountry: 'France',
+        winner: 'France',
+        runnerUp: 'Brazil',
+    },
+    '2002': {
+        hostCountry: 'South Korea, Japan',
+        winner: 'Brazil',
+        runnerUp: 'Germany',
+    },
+    '2006': {
+        hostCountry: 'Germany',
+        winner: 'Italy',
+        runnerUp: 'France',
+    },
+    '2010': {
+        hostCountry: 'South Africa',
+        winner: 'Spain',
+        runnerUp: 'Netherlands',
+    },
+    '2014': {
+        hostCountry: 'Brazil',
+        winner: 'Germany',
+        runnerUp: 'Argentina',
+    },
+};
+
+module.exports = worldCups;
+
