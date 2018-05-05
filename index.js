978148b74ba96447baef8e932e4912811f2ab196
Jibola Sogbein <ajibolasogbein@gmail.com>
2018-05-05 17:19:24 -0500

    Add highest goalscorers

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ var worldCups = {
         hostCountry: 'Uruguay',
         winner: 'Uruguay',
         runnerUp: 'Argentina',
+        topGoalScorer: [{ name: 'Guillermo Stábile', country: 'Argentina', numberOfGoals: 8 }],
         totalAttendance: 590549,
-        numberOfMatches: 18
+        numberOfMatches: 18,
     },
     '1934': {
         hostCountry: 'Italy',
         winner: 'Italy',
         runnerUp: ' Czechoslovakia',
+        topGoalScorer: [{ name: 'Oldřich Nejedlý', country: 'Czechoslovakia', numberOfGoals: 5 }],
         totalAttendance: 363000,
         numberOfMatches: 17
     },
@@ -17,6 +19,7 @@ var worldCups = {
         hostCountry: 'France',
         winner: 'Italy',
         runnerUp: 'Hungary',
+        topGoalScorer: [{ name: 'Leônidas', country: 'Brazil', numberOfGoals: 7 }],
         totalAttendance: 375000,
         numberOfMatches: 18
     },
@@ -24,6 +27,7 @@ var worldCups = {
         hostCountry: 'Brazil',
         winner: 'Uruguay',
         runnerUp: 'Brazil',
+        topGoalScorer: [{ name: 'Ademir', country: 'Brazil', numberOfGoals: 8 }],
         totalAttendance: 1045246,
         numberOfMatches: 22
     },
@@ -31,6 +35,7 @@ var worldCups = {
         hostCountry: 'Switzerland',
         winner: 'West Germany',
         runnerUp: 'Hungary',
+        topGoalScorer: [{ name: 'Sándor Kocsis', country: 'Hungary', numberOfGoals: 11 }],
         totalAttendance: 768607,
         numberOfMatches: 26
     },
@@ -38,6 +43,7 @@ var worldCups = {
         hostCountry: 'Sweden',
         winner: 'Brazil',
         runnerUp: 'Sweden',
+        topGoalScorer: [{ name: 'Just Fontaine', country: 'France', numberOfGoals: 13 }],
         totalAttendance: 819810,
         numberOfMatches: 35
     },
@@ -45,6 +51,14 @@ var worldCups = {
         hostCountry: 'Chile',
         winner: 'Brazil',
         runnerUp: 'Czechoslovakia',
+        topGoalScorer: [
+                            { name: 'Garrincha', country: 'Brazil', numberOfGoals: 4 },
+                            { name: 'Vavá', country: 'Brazil', numberOfGoals: 4 },
+                            { name: 'Leonel Sánchez', country: 'Chile', numberOfGoals: 4 },
+                            { name: 'Flórián Albert', country: 'Hungary', numberOfGoals: 4 },
+                            { name: 'Valentin Ivanov', country: 'Soviet Union', numberOfGoals: 4 },
+                            { name: 'Dražan Jerković', country: 'Yugoslavia', numberOfGoals: 4 },
+                        ],
         totalAttendance: 893172,
         numberOfMatches: 32
     },
@@ -52,6 +66,7 @@ var worldCups = {
         hostCountry: 'England',
         winner: 'England',
         runnerUp: 'West Germany',
+        topGoalScorer: [{ name: 'Eusébio', country: 'Portugal', numberOfGoals: 9 }],
         totalAttendance: 1563135,
         numberOfMatches: 32
     },
@@ -59,6 +74,7 @@ var worldCups = {
         hostCountry: 'Mexico',
         winner: 'Brazil',
         runnerUp: 'Italy',
+        topGoalScorer: [{ name: 'Gerd Müller', country: 'West Germany', numberOfGoals: 10 }],
         totalAttendance: 1603975,
         numberOfMatches: 32
     },
@@ -66,6 +82,7 @@ var worldCups = {
         hostCountry: 'West Germany',
         winner: 'West Germany',
         runnerUp: 'Netherlands',
+        topGoalScorer: [{ name: 'Grzegorz Lato', country: 'Poland', numberOfGoals: 7 }],
         totalAttendance: 1865753,
         numberOfMatches: 38
     },
@@ -73,6 +90,7 @@ var worldCups = {
         hostCountry: 'Argentina',
         winner: 'Argentina',
         runnerUp: 'Netherlands',
+        topGoalScorer: [{ name: 'Mario Kempes', country: 'Argentina', numberOfGoals: 6 }],
         totalAttendance: 1545791,
         numberOfMatches: 38
     },
@@ -80,6 +98,7 @@ var worldCups = {
         hostCountry: 'Spain',
         winner: 'Italy',
         runnerUp: 'West Germany',
+        topGoalScorer: [{ name: 'Paolo Rossi', country: 'Italy', numberOfGoals: 6 }],
         totalAttendance: 2109723,
         numberOfMatches: 52
     },
@@ -87,6 +106,7 @@ var worldCups = {
         hostCountry: 'Mexico',
         winner: 'Argentina',
         runnerUp: 'West Germany',
+        topGoalScorer: [{ name: 'Gary Lineker', country: 'England', numberOfGoals: 6 }],
         totalAttendance: 239403,
         numberOfMatches: 52
     },
@@ -94,6 +114,7 @@ var worldCups = {
         hostCountry: 'Italy',
         winner: 'West Germany',
         runnerUp: 'Argentina',
+        topGoalScorer: [{ name: 'Salvatore Schillaci', country: 'Italy', numberOfGoals: 6 }],
         totalAttendance: 2516215,
         numberOfMatches: 52
     },
@@ -101,6 +122,7 @@ var worldCups = {
         hostCountry: 'United States',
         winner: 'Brazil',
         runnerUp: 'Italy',
+        topGoalScorer: [{ name: 'Oleg Salenko', country: 'Russia', numberOfGoals: 6 }, { name: 'Hristo Stoichkov', country: 'Bulgaria', numberOfGoals: 6 }],
         totalAttendance: 3587538,
         numberOfMatches: 52
     },
@@ -108,13 +130,15 @@ var worldCups = {
         hostCountry: 'France',
         winner: 'France',
         runnerUp: 'Brazil',
+        topGoalScorer: [{ name: 'Davor Šuker', country: 'Italy', numberOfGoals: 6 }],
         totalAttendance: 2785100,
         numberOfMatches: 64
     },
     '2002': {
-        hostCountry: 'South Korea, Japan',
+        hostCountry: 'South Korea,Japan',
         winner: 'Brazil',
         runnerUp: 'Germany',
+        topGoalScorer: [{ name: 'Ronaldo', country: 'Brazil', numberOfGoals: 8 }],
         totalAttendance: 2705197,
         numberOfMatches: 64
     },
@@ -122,6 +146,7 @@ var worldCups = {
         hostCountry: 'Germany',
         winner: 'Italy',
         runnerUp: 'France',
+        topGoalScorer: [{ name: 'Miroslav Klose', country: 'Germany', numberOfGoals: 5 }],
         totalAttendance: 3359439,
         numberOfMatches: 64
     },
@@ -129,6 +154,12 @@ var worldCups = {
         hostCountry: 'South Africa',
         winner: 'Spain',
         runnerUp: 'Netherlands',
+        topGoalScorer: [
+                            { name: 'Diego Forlán', country: 'Uruguay', numberOfGoals: 5 },
+                            { name: 'Thomas Müller', country: 'Germany', numberOfGoals: 5 },
+                            { name: 'Wesley Sneijder', country: 'Netherlands', numberOfGoals: 5 },
+                            { name: 'David Villa', country: 'Spain', numberOfGoals: 5 }
+                        ],
         totalAttendance: 3178856,
         numberOfMatches: 64
     },
@@ -136,6 +167,7 @@ var worldCups = {
         hostCountry: 'Brazil',
         winner: 'Germany',
         runnerUp: 'Argentina',
+        topGoalScorer: [{ name: 'James Rodríguez', country: 'Colombia', numberOfGoals: 6 }],
         totalAttendance: 3429873,
         numberOfMatches: 64
     },
